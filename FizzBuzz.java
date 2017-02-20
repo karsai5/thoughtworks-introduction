016d35423d764127fb155b01c62405e03c2be12e
Linus Karsai <karsai5@gmail.com>
2017-02-20 15:52:01 +1100

    Added FizzBuzz code.

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+/**
+ * Created by linus on 20/2/17.
+ */
+public class FizzBuzz {
+
+    public void printCount(int i) {
+        for (int x = 1; x <= i; x++){
+            if (x % 3 == 0 && x % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if (x % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (x % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(x);
+            }
+        }
+    }
+}
---
+++ FizzBuzzTests.java
@@ -0,0 +1,59 @@
+import org.junit.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by linus on 20/2/17.
+ */
+public class FizzBuzzTests {
+    // used for checking output of System.out.println
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    FizzBuzz fb;
+
+    @Before
+    public void setUpStreams() {
+        // create new triangle
+        fb = new FizzBuzz();
+        // reset streams for checking output
+        resetStreams();
+    }
+
+    @After
+    public void cleanUpStreams() {
+        // remove streams for checking output
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    public void resetStreams() {
+        // create streams for checking output
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+        outContent.reset();
+        errContent.reset();
+    }
+
+    @Test
+    public void count_to_three() {
+        fb.printCount(3);
+        assertEquals("1\n2\nFizz\n", outContent.toString());
+    }
+
+    @Test
+    public void count_to_five() {
+        fb.printCount(5);
+        assertEquals("1\n2\nFizz\n4\nBuzz\n", outContent.toString());
+    }
+
+    @Test
+    public void count_to_fifteen() {
+        fb.printCount(15);
+        assertEquals("1\n" + "2\n" + "Fizz\n" + "4\n" + "Buzz\n" + "Fizz\n" + "7\n" + "8\n" + "Fizz\n" +
+                "Buzz\n" + "11\n" + "Fizz\n" + "13\n" + "14\n" + "FizzBuzz\n", outContent.toString());
+    }
+
+}
